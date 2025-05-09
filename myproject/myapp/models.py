a538f4c960bdd769519917b1fdf9d8940f976370
naufal741 <mandanrinauta@gmail.com>
2025-05-10 02:06:44 +0700

    Update models.py

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -6,11 +6,3 @@ class StatusModel(models.Model):
 
     def __str__(self):
         return self.name
-
-class JobTitle(models.Model):
-    name = models.CharField(max_length = 200)
-    description = models.TextField()
-    status = models.ForeignKey(StatusModel,
-        related_name = 'status_status_model',
-        blank = True, null =True,
-        on_delete = models.SET_NULL)
